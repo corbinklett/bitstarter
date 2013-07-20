c08d324a0a99aadd7b093d5e58a62eae2809462e
Corbin Klett <corbinklett@gmail.com>
2013-07-20 15:28:47 -0400

    nabar changes

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// var navbar = $('.ah-nav');
+
+
+
+// $(document).scroll(function(e) {
+// 	navbar.addClass('ah-nav-scrolled');
+// 	console.log('scrolled');
+// });
+
+
+ $(window).scroll(function(){
+        var scrollTop = $(window).scrollTop();
+        if(scrollTop != 0)
+            $('.ah-nav').stop().animate({'opacity': '.9'},400);
+      	else   
+            $('.ah-nav').stop().animate({'opacity':'.4'},400); 
+    });
+
+function showMenu() {
+	var ahnav = $('.ah-nav');
+	if(ahnav.css('height') == '90px') {
+		ahnav.stop().animate({'height': '200px'}, 300);
+		$('.ah-nav-contents').stop().animate({'height': '200px'}, 300);
+	}
+	else {
+		ahnav.stop().animate({'height': '90px'}, 300);
+		$('.ah-nav-contents').stop().animate({'height': '90px'}, 300);		
+	}
+
+}
